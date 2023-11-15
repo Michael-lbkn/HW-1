1ed61562d0d10761a7138e2ea25f78ff3bec618e
michael-lbkn <michael.lbkn@gmail.com>
2023-11-15 13:42:32 +0200

    update

--- Lesson and hw #3/HW #3.py
+++ Lesson and hw #3/HW #3.py
@@ -21,27 +21,79 @@
 
 # Рішення:
 
-shopping_cart = []
+txt_to_display_initial = "Shopping Cart: "
+txt_to_display_when_delete = "Write product number to delete: "
 
-for _ in range(5):
-    item = input("Enter an item: ")
-    shopping_cart.append(item)
+list_a = input(txt_to_display_initial)
+list_a = list_a.replace(', ', ',').split(",")
 
-print("Your current shopping cart:")
-print(shopping_cart)
+if len(list_a):
+    prod_num_to_delete = int(input(txt_to_display_when_delete)) - 1
+    if prod_num_to_delete <= len(list_a) - 1 and prod_num_to_delete >= 0:
+        list_a.pop(prod_num_to_delete)
+    print(f'Products list: {list_a}')
 
-while len(shopping_cart) > 0:
-    remove_choice = input("Do you want to remove an item from the cart? (yes/no): ").lower()
+if len(list_a):
+    prod_num_to_delete = int(input(txt_to_display_when_delete)) - 1
+    if prod_num_to_delete <= len(list_a) - 1 and prod_num_to_delete >= 0:
+        list_a.pop(prod_num_to_delete)
+    print(f'Products list: {list_a}')
 
-    if remove_choice == 'yes':
+if len(list_a):
+    prod_num_to_delete = int(input(txt_to_display_when_delete)) - 1
+    if prod_num_to_delete <= len(list_a) - 1 and prod_num_to_delete >= 0:
+        list_a.pop(prod_num_to_delete)
+    print(f'Products list: {list_a}')
 
-        removed_item = shopping_cart.pop(0)
-        print(f"Removed {removed_item} from the cart.")
-        print("Updated shopping cart:", shopping_cart)
-    else:
-        break
+if len(list_a):
+    prod_num_to_delete = int(input(txt_to_display_when_delete)) - 1
+    if prod_num_to_delete <= len(list_a) - 1 and prod_num_to_delete >= 0:
+        list_a.pop(prod_num_to_delete)
+    print(f'Products list: {list_a}')
 
-if not shopping_cart:
-    print("Your shopping cart is empty.")
+if len(list_a):
+    prod_num_to_delete = int(input(txt_to_display_when_delete)) - 1
+    if prod_num_to_delete <= len(list_a) - 1 and prod_num_to_delete >= 0:
+        list_a.pop(prod_num_to_delete)
+    print(f'Products list: {list_a}')
+
+
+if len(list_a):
+    print("Products list is NOT empty\n")
 else:
-    print("Final Shopping Cart:", shopping_cart)
+    print("Products list is EMPTY\n")
+
+
+new_prods = input('Write more products: ')
+new_prods_list = new_prods.replace(', ', ',').split(',')
+
+list_a.extend(new_prods_list)
+
+print('Products list: ', list_a)
+print('BYE!')
+
+# ===================================================================================================================
+
+# shopping_cart = []
+# for _ in range(5):
+# item = input("Enter an item: ")
+# shopping_cart.append(item)
+
+# print("Your current shopping cart:")
+# print(shopping_cart)
+
+# while len(shopping_cart) > 0:
+# remove_choice = input("Do you want to remove an item from the cart? (yes/no): ").lower()
+
+# if remove_choice == 'yes':
+
+# removed_item = shopping_cart.pop(0)
+# print(f"Removed {removed_item} from the cart.")
+# print("Updated shopping cart:", shopping_cart)
+#  else:
+# break
+
+# if not shopping_cart:
+# print("Your shopping cart is empty.")
+# else:
+# print("Final Shopping Cart:", shopping_cart)
